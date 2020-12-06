30ee7815debd4dbc741086aa0c70f79fc9cd371b
Travis Reyburn <travisreyburn@gmail.com>
2020-12-05 22:07:54 -0700

    loading file

--- Day2/cmd/main.go
+++ Day2/cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"github.com/TReyburn/advent-of-go/common/fileHandler"
+	"log"
+)
+
 func main() {
-	
+	pws, err := fileHandler.LoadDay2File("assets/input.txt")
+	if err != nil {
+		log.Fatalln("Error loading passwords", err)
+	}
+	fmt.Println(pws)
 }
