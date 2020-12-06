c9673b794a2afbd7381aed7ddece601f805f5a60
Travis Reyburn <travisreyburn@gmail.com>
2020-12-05 22:06:47 -0700

    implemented struct and validate receiver

---
+++ Day2/password/password.go
@@ -0,0 +1,28 @@
+package password
+
+import (
+	"sort"
+	"strings"
+)
+
+type Password struct {
+	Required string
+	Low int
+	High int
+	Password string
+}
+
+func (p Password) Validate() bool {
+	str := strings.Split(p.Password, "")
+	sort.Strings(str)
+	count := 0
+	for _, s := range str {
+		if s == p.Required {
+			count++
+		}
+		if count > 0 && s != p.Required {
+			return true
+		}
+	}
+	return false
+}
