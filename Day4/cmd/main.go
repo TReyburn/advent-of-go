4cb4d2dbe66dd29bb92f75215a3b3eed526f1dbb
Travis Reyburn <treyburn@alteryx.com>
2020-12-07 16:40:49 -0700

    implemented solution to part 1

--- Day4/cmd/main.go
+++ Day4/cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"github.com/TReyburn/advent-of-go/Day4/passport"
+	"github.com/TReyburn/advent-of-go/common/filehandler"
+	"log"
+)
+
 func main() {
-	$END$
+	var required = []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"}
+	ps := passport.NewPassportScanner()
+	err := filehandler.LoadInputFile("assets/input.txt", ps)
+	if err != nil {
+		log.Fatalln("Error reading file:", err)
+	}
+	res := ps.ValidatePassports(required)
+	fmt.Println(res)
 }
