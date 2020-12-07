7c40a45f7c3a8f811bb18e588279c482f80397b9
Travis Reyburn <travisreyburn@gmail.com>
2020-12-06 19:45:10 -0700

    added more test cases

--- Day3/traverse/traverse_test.go
+++ Day3/traverse/traverse_test.go
@@ -16,10 +16,42 @@ var testData = []string{
 	".#..#...#.#",
 }
 
-func TestDumbTraverse(t *testing.T) {
+func TestDumbTraverseCase1(t *testing.T) {
 	count := Traverse(testData, "#", 1, 3)
 
 	if count != 7 {
 		t.Error("Expected 7; got", count)
+	}
+}
+
+func TestDumbTraverseCase2(t *testing.T) {
+	count := Traverse(testData, "#", 1, 1)
+
+	if count != 2 {
+		t.Error("Expected 2; got", count)
+	}
+}
+
+func TestDumbTraverseCase3(t *testing.T) {
+	count := Traverse(testData, "#", 1, 5)
+
+	if count != 3 {
+		t.Error("Expected 3; got", count)
+	}
+}
+
+func TestDumbTraverseCase4(t *testing.T) {
+	count := Traverse(testData, "#", 1, 7)
+
+	if count != 4 {
+		t.Error("Expected 4; got", count)
+	}
+}
+
+func TestDumbTraverseCase5(t *testing.T) {
+	count := Traverse(testData, "#", 2, 1)
+
+	if count != 2 {
+		t.Error("Expected 2; got", count)
 	}
 }
