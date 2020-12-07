b2eef378b7c5815337061a1b2f9aa43b7de08275
Travis Reyburn <treyburn@alteryx.com>
2020-12-07 16:57:41 -0700

    byr validator

--- Day4/validator/validator.go
+++ Day4/validator/validator.go
@@ -1,1 +1,19 @@
 package validator
+
+import "strconv"
+
+type Validator struct {
+	Validators []map[string]func(string)(bool, error)
+}
+
+// byr (Birth Year) - four digits; at least 1920 and at most 2002.
+func byrValidate(byr string) (bool, error) {
+	if len(byr) != 4 {
+		return false, nil
+	}
+	nByr, err := strconv.Atoi(byr)
+	if err != nil {
+		return false, err
+	}
+	return nByr >= 1920 && nByr <= 2002, nil
+}
--- Day4/validator/validator_test.go
+++ Day4/validator/validator_test.go
@@ -1,1 +1,29 @@
 package validator
+
+import "testing"
+
+func TestByrValidatePos(t *testing.T) {
+	v := "2002"
+
+	res, err := byrValidate(v)
+	if err != nil {
+		t.Error("Unexpected error", err)
+	}
+
+	if res != true {
+		t.Error("Failed to validate byr:2002")
+	}
+}
+
+func TestByrValidateNeg(t *testing.T) {
+	iv := "2003"
+
+	res, err := byrValidate(iv)
+	if err != nil {
+		t.Error("Unexpected error", err)
+	}
+
+	if res != false {
+		t.Error("Failed to invalidate byr:2003")
+	}
+}
