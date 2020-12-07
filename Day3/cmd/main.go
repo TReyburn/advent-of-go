2417100414a4fe314f20b792b725eee7bff5d8b3
Travis Reyburn <travisreyburn@gmail.com>
2020-12-06 19:56:36 -0700

    implemented part 2 solution

--- Day3/cmd/main.go
+++ Day3/cmd/main.go
@@ -14,4 +14,14 @@ func main() {
 	}
 	res := traverse.Traverse(data, "#", 1, 3)
 	fmt.Println("We hit", res, "trees")
+
+	angles := [][]int{
+		[]int{1,1},
+		[]int{1,3},
+		[]int{1,5},
+		[]int{1,7},
+		[]int{2,1},
+	}
+	fres := traverse.MultiTraverse(data, "#", angles)
+	fmt.Println("Multitraverse result of", fres)
 }
