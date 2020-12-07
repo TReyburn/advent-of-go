de3d1b4ad1b7d6aa452045e5628037c17c02b849
Travis Reyburn <travisreyburn@gmail.com>
2020-12-06 19:13:04 -0700

    setting uup main

--- Day3/cmd/main.go
+++ Day3/cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"github.com/TReyburn/advent-of-go/common/fileHandler"
+	"log"
+)
+
 func main() {
-	
+	data, err := fileHandler.LoadDay3File("assets/input.txt")
+	if err != nil {
+		log.Fatalln("Error opening file", err)
+	}
+	fmt.Println(data)
 }
