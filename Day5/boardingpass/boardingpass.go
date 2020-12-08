a927d9ddd616de500ce7284f1edb052c2e613f3c
Travis Reyburn <treyburn@alteryx.com>
2020-12-07 20:02:38 -0700

    implemented writer

--- Day5/boardingpass/boardingpass.go
+++ Day5/boardingpass/boardingpass.go
@@ -1,2 +1,36 @@
 package boardingpass
 
+import (
+	"bytes"
+	"strings"
+)
+
+type BPManager struct {
+	Passes []BoardingPass
+}
+
+func (bpm *BPManager) Write(p []byte) (int, error) {
+	rb := len(p)
+	bss := bytes.Split(p, []byte{13})
+	for _, bString := range bss {
+		rawString := string(bString)
+		rawString = strings.Trim(rawString, "\n")
+		bp := BoardingPass{
+			Code: rawString,
+		}
+		bpm.Passes = append(bpm.Passes, bp)
+	}
+	return rb, nil
+}
+
+func NewBoardingPassManager() *BPManager {
+	bpm := BPManager{Passes: make([]BoardingPass, 0)}
+	return &bpm
+}
+
+type BoardingPass struct {
+	Code string
+	Row int
+	Column int
+	ID int
+}
--- Day5/cmd/main.go
+++ Day5/cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/TReyburn/advent-of-go/Day5/boardingpass"
+	"github.com/TReyburn/advent-of-go/common/filehandler"
+	"log"
+)
 
+func main() {
+	bpm := boardingpass.NewBoardingPassManager()
+	err := filehandler.LoadInputFile("assets/input.txt", bpm)
+	if err != nil {
+		log.Fatalln("Unexpected error loading file", err)
+	}
+	fmt.Println(bpm.Passes)
 }
