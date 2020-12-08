20e1d81e9eb234df41bbf4b5fb70ed0331572993
Travis Reyburn <treyburn@alteryx.com>
2020-12-08 15:20:20 -0700

    implemented part 1 solution

--- Day6/cmd/main.go
+++ Day6/cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"github.com/TReyburn/advent-of-go/Day6/customs"
+	"github.com/TReyburn/advent-of-go/common/filehandler"
+	"log"
+)
+
 func main() {
-	$END$
+	vs := customs.NewVisaScanner()
+	err := filehandler.LoadInputFile("assets/input.txt", vs)
+	if err != nil {
+		log.Fatalln("Unexpected error loading file:", err)
+	}
+	fmt.Println("Sum of Visa counts (old):", vs.SumVisas())
 }
