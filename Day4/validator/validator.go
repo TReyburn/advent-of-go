b8e95fee1b66a438550af56269fc37e1e6c388f5
Travis Reyburn <treyburn@alteryx.com>
2020-12-07 17:06:58 -0700

    added iyr validator and tests

--- Day4/validator/validator.go
+++ Day4/validator/validator.go
@@ -3,7 +3,7 @@ package validator
 import "strconv"
 
 type Validator struct {
-	Validators []map[string]func(string)(bool, error)
+	Validators map[string]func(string)(bool, error)
 }
 
 // byr (Birth Year) - four digits; at least 1920 and at most 2002.
@@ -16,4 +16,16 @@ func byrValidate(byr string) (bool, error) {
 		return false, err
 	}
 	return nByr >= 1920 && nByr <= 2002, nil
+}
+
+// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+func iyrValidate(iyr string) (bool, error) {
+	if len(iyr) != 4 {
+		return false, nil
+	}
+	nIyr, err := strconv.Atoi(iyr)
+	if err != nil {
+		return false, err
+	}
+	return nIyr >= 2010 && nIyr <= 2020, nil
 }
--- Day4/validator/validator_test.go
+++ Day4/validator/validator_test.go
@@ -27,3 +27,29 @@ func TestByrValidateNeg(t *testing.T) {
 		t.Error("Failed to invalidate byr:2003")
 	}
 }
+
+func TestIyrValidatePos(t *testing.T) {
+	v := "2010"
+
+	res, err := iyrValidate(v)
+	if err != nil {
+		t.Error("Unexpected error", err)
+	}
+
+	if res != true {
+		t.Error("Failed to validate iyr:2010")
+	}
+}
+
+func TestIyrValidateMeh(t *testing.T) {
+	iv := "2009"
+
+	res, err := iyrValidate(iv)
+	if err != nil {
+		t.Error("Unexpected error", err)
+	}
+
+	if res != false {
+		t.Error("Failed to invalidate iyr:2009")
+	}
+}
