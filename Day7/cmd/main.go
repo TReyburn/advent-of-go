7a6cc5f972efa1a9395ace156bc89cce54179c87
Travis Reyburn <treyburn@alteryx.com>
2020-12-09 08:15:46 -0700

    added basic notes of algo implementation

--- Day7/cmd/main.go
+++ Day7/cmd/main.go
@@ -1,5 +1,11 @@
 package main
 
+// Algo steps
+// 1, load file as [][]string
+// 2. iterate over each string
+// 3. parse each string and load into graph - need to figure out the graph bit...
+// 3. iterate over each graph node (skip shiny)
+// 4. do breadth first search of shiny golden
 func main() {
-	$END$
+
 }
