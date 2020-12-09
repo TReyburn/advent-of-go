323b2edc07dfe0a30944f672d0a14a0f5d3a6ca1
Travis Reyburn <treyburn@alteryx.com>
2020-12-09 09:23:54 -0700

    added AddNode tests

--- Day7/graph/graph_test.go
+++ Day7/graph/graph_test.go
@@ -1,1 +1,29 @@
 package graph
+
+import "testing"
+
+func TestGraph_AddNode(t *testing.T) {
+	g := NewGraph()
+	n := NewNode("Test")
+	n2 := NewNode("Test2")
+
+	g.AddNode(n)
+	g.AddNode(n2)
+
+	if len(g.Nodes) != 2 {
+		t.Error("Expected Nodes len of 2; got", len(g.Nodes))
+	}
+}
+
+func TestGraph_AddNodeDuplicates(t *testing.T) {
+	g := NewGraph()
+	n := NewNode("Test")
+	n2 := NewNode("Test")
+
+	g.AddNode(n)
+	g.AddNode(n2)
+
+	if len(g.Nodes) != 1 {
+		t.Error("Expected Nodes len of 1; got", len(g.Nodes))
+	}
+}
