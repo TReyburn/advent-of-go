3a7033de1a1384140ddd4ae42c2825d2641d5160
Travis Reyburn <treyburn@alteryx.com>
2020-12-09 09:27:11 -0700

    AddEdge tests

--- Day7/graph/graph_test.go
+++ Day7/graph/graph_test.go
@@ -27,3 +27,39 @@ func TestGraph_AddNodeDuplicates(t *testing.T) {
 		t.Error("Expected Nodes len of 1; got", len(g.Nodes))
 	}
 }
+
+func TestGraph_AddEdge(t *testing.T) {
+	g := NewGraph()
+	n := NewNode("Test")
+	n2 := NewNode("Test2")
+	cost := 5
+
+	g.AddEdge(n, n2, cost)
+
+	if len(g.Nodes) != 2 {
+		t.Error("Expected Nodes len of 2; got", len(g.Nodes))
+	}
+
+	if len(g.Edges) != 1 {
+		t.Error("Expected edges len of 1; got", len(g.Edges))
+	}
+}
+
+func TestGraph_AddEdgeDuplicateNodes(t *testing.T) {
+	g := NewGraph()
+	n := NewNode("Test")
+	n2 := NewNode("Test2")
+	n3 := NewNode("Test3")
+	cost := 5
+
+	g.AddEdge(n, n2, cost)
+	g.AddEdge(n2, n3, cost)
+
+	if len(g.Nodes) != 3 {
+		t.Error("Expected Nodes len of 3; got", len(g.Nodes))
+	}
+
+	if len(g.Edges) != 2 {
+		t.Error("Expected edges len of 2; got", len(g.Edges))
+	}
+}
