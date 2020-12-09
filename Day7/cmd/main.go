3bd45e6eee2fe395c567e03f1738271abdf2de79
Travis Reyburn <treyburn@alteryx.com>
2020-12-09 12:01:06 -0700

    implemented part 1 solution

--- Day7/cmd/main.go
+++ Day7/cmd/main.go
@@ -1,11 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"github.com/TReyburn/advent-of-go/Day7/graph"
+	"github.com/TReyburn/advent-of-go/common/filehandler"
+	"log"
+)
+
 // Algo steps
-// 1, load file as [][]string
+// 1, load file as []string
 // 2. iterate over each string
-// 3. parse each string and load into graph - need to figure out the graph bit...
+// 3. parse each string and load into graph
 // 3. iterate over each graph node (skip shiny)
 // 4. do breadth first search of shiny golden
 func main() {
-
+	g := graph.NewGraph()
+	err := filehandler.LoadInputFile("assets/input.txt", g)
+	if err != nil {
+		log.Fatalln("Unexpected err:", err)
+	}
+	res := g.CountPossiblePaths("shiny gold bag")
+	fmt.Println("Count of bags which can eventual contain a shiny gold bag:", res)
 }
