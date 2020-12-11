14d3d84a7cc34c7bf6a13ff24270df95036e7e2f
Travis Reyburn <travisreyburn@gmail.com>
2020-12-10 20:13:41 -0700

    implemented part 1 solution

--- Day8/cmd/main.go
+++ Day8/cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"github.com/TReyburn/advent-of-go/Day8/console"
+	"github.com/TReyburn/advent-of-go/common/filehandler"
+	"log"
+)
+
 func main() {
-	
+	c := console.NewConsole()
+	err := filehandler.LoadInputFile("assets/input.txt", c)
+	if err != nil {
+		log.Fatalln("Unexpected error reading file:", err)
+	}
+	res := c.Run()
+	fmt.Println("Accumulator amount before repeat:", res)
 }
