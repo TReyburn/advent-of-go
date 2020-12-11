09e5f2fab5e9fd15b889f23480c8f6230c1b8493
Travis Reyburn <treyburn@alteryx.com>
2020-12-11 16:29:09 -0700

    added summarize func and test

--- Day10/adapter/adapter.go
+++ Day10/adapter/adapter.go
@@ -1,6 +1,9 @@
 package adapter
 
-import "sort"
+import (
+	"errors"
+	"sort"
+)
 
 type Adapter struct {
 	SortedArray []int
@@ -11,6 +14,23 @@ func (a *Adapter) Load(ns []int) {
 	a.SortedArray = append(a.SortedArray, ns...)
 	max := a.SortedArray[len(a.SortedArray)-1] + 3
 	a.SortedArray= append(a.SortedArray, max)
+}
+
+func (a *Adapter) Summarize() (map[int]int, error) {
+	res := map[int]int{1:0, 2:0, 3:0}
+	prev := 0
+	for _, val := range a.SortedArray[1:] {
+		diff := val - prev
+		if diff > 3 {
+			return nil, errors.New("unable to link adapters")
+		}
+		if diff == 0 {
+			return nil, errors.New("unable to use all adapters")
+		}
+		res[diff]++
+		prev = val
+	}
+	return res, nil
 }
 
 func NewAdapter() *Adapter {
--- Day10/adapter/adapter_test.go
+++ Day10/adapter/adapter_test.go
@@ -12,3 +12,17 @@ func TestAdapter_Load(t *testing.T) {
 
 	assert.Equal(t, []int{0,1,2,3,4,7}, a.SortedArray)
 }
+
+func TestAdapter_Summarize(t *testing.T) {
+	ns := []int{16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4}
+	a := NewAdapter()
+	a.Load(ns)
+
+	res, err := a.Summarize()
+	if err != nil {
+		t.Error("Unexpected error while summarizing jolt jumps:", err)
+	}
+
+	assert.Equal(t, 7, res[1])
+	assert.Equal(t, 5, res[3])
+}
