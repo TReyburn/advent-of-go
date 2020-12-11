7349de60a4b2e03b47e7afd4c1f3ab6812b67dae
Travis Reyburn <travisreyburn@gmail.com>
2020-12-10 23:06:58 -0700

    implemented solution to part 2

--- Day8/cmd/main.go
+++ Day8/cmd/main.go
@@ -9,10 +9,18 @@ import (
 
 func main() {
 	c := console.NewConsole()
-	err := filehandler.LoadInputFile("assets/input.txt", c)
+	err := filehandler.LoadInputFile("C:/Users/Travis/advent-of-go/Day8/assets/input.txt", c)
 	if err != nil {
 		log.Fatalln("Unexpected error reading file:", err)
 	}
 	res := c.Run()
 	fmt.Println("Accumulator amount before repeat:", res)
+
+	c1 := console.NewConsole()
+	err = filehandler.LoadInputFile("C:/Users/Travis/advent-of-go/Day8/assets/input.txt", c1)
+	if err != nil {
+		log.Fatalln("Unexpected error reading file:", err)
+	}
+	res2 := c1.DFSDebug()
+	fmt.Println("Accumulator after fixed bug:", res2)
 }
