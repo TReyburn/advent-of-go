6b228314013f9cd4bbf6d50eea2fb158e1dd40e6
Travis Reyburn <treyburn@alteryx.com>
2020-12-11 14:54:55 -0700

    added shift tests

--- Day9/xmas/xmas_test.go
+++ Day9/xmas/xmas_test.go
@@ -50,3 +50,73 @@ func TestDecoder_AddKey(t *testing.T) {
 	assert.Equal(t, 1, len(d.Preamble))
 	assert.Equal(t, false, d.Preamble[1])
 }
+
+func TestDecoder_Shift(t *testing.T) {
+	ns := []int{0, 1, 2, 3, 4}
+	d := NewDecoder(2)
+	err := d.Load(ns)
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+
+	assert.Equal(t, 2, len(d.Preamble))
+	assert.Equal(t, 2, len(d.Queue.Queue))
+	assert.Equal(t, 3, len(d.Remainder))
+	assert.Equal(t, map[int]bool{0:false, 1:false}, d.Preamble)
+	assert.Equal(t, []int{0, 1}, d.Queue.Queue)
+	assert.Equal(t, []int{2, 3, 4}, d.Remainder)
+
+	err = d.Shift()
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	assert.Equal(t, 2, len(d.Preamble))
+	assert.Equal(t, 2, len(d.Queue.Queue))
+	assert.Equal(t, 2, len(d.Remainder))
+	assert.Equal(t, map[int]bool{1:false, 2:false}, d.Preamble)
+	assert.Equal(t, []int{1, 2}, d.Queue.Queue)
+	assert.Equal(t, []int{3, 4}, d.Remainder)
+}
+
+func TestDecoder_ShiftLastShift(t *testing.T) {
+	ns := []int{0, 1, 2}
+	d := NewDecoder(2)
+	err := d.Load(ns)
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+
+	assert.Equal(t, 2, len(d.Preamble))
+	assert.Equal(t, 2, len(d.Queue.Queue))
+	assert.Equal(t, 1, len(d.Remainder))
+	assert.Equal(t, map[int]bool{0:false, 1:false}, d.Preamble)
+	assert.Equal(t, []int{0, 1}, d.Queue.Queue)
+	assert.Equal(t, []int{2}, d.Remainder)
+
+	err = d.Shift()
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	assert.Equal(t, 2, len(d.Preamble))
+	assert.Equal(t, 2, len(d.Queue.Queue))
+	assert.Equal(t, 0, len(d.Remainder))
+	assert.Equal(t, map[int]bool{1:false, 2:false}, d.Preamble)
+	assert.Equal(t, []int{1, 2}, d.Queue.Queue)
+	assert.Equal(t, []int{}, d.Remainder)
+}
+
+func TestDecoder_ShiftCantShift(t *testing.T) {
+	ns := []int{0, 1, 2}
+	d := NewDecoder(2)
+	err := d.Load(ns)
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+
+	err = d.Shift()
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	err = d.Shift()
+	assert.Error(t, err, "no remainder left to shift")
+}
