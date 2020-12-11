d50452bc106e7b940e658887211e9d544028d06a
Travis Reyburn <treyburn@alteryx.com>
2020-12-11 15:24:33 -0700

    implemented part 1 solution

--- Day9/cmd/main.go
+++ Day9/cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"github.com/TReyburn/advent-of-go/Day9/xmas"
+	"github.com/TReyburn/advent-of-go/common/filehandler"
+	"log"
+)
+
 func main() {
-	
+	d := xmas.NewDecoder(25)
+	err := filehandler.LoadInputFile("assets/input.txt", d)
+	if err != nil {
+		log.Fatalln("Unexpected error loading file:", err)
+	}
+
+	res, err := d.Attack()
+	if err != nil {
+		log.Fatalln("Unexpected error while attacking cipher:", err)
+	}
+
+	fmt.Println("Found non-compliant int:", res)
 }
