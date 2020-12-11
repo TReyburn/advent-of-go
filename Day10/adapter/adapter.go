c0c76785ede216c3336e4e27a5943d3f0973d005
Travis Reyburn <treyburn@alteryx.com>
2020-12-11 16:15:08 -0700

    early struct layout

--- Day10/adapter/adapter.go
+++ Day10/adapter/adapter.go
@@ -1,1 +1,19 @@
 package adapter
+
+import "sort"
+
+type Adapter struct {
+	SortedArray []int
+}
+
+func (a *Adapter) Load(ns []int) {
+	sort.Ints(ns)
+	a.SortedArray = append(a.SortedArray, ns...)
+	max := a.SortedArray[len(ns)-1] + 3
+	a.SortedArray= append(a.SortedArray, max)
+}
+
+func NewAdapter() *Adapter {
+	a := Adapter{SortedArray: []int{0}}
+	return &a
+}
