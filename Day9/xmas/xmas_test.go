543d4d7d949545465ec011f573924ebb1f582280
Travis Reyburn <treyburn@alteryx.com>
2020-12-11 14:18:35 -0700

    added queue tests

--- Day9/xmas/xmas_test.go
+++ Day9/xmas/xmas_test.go
@@ -1,1 +1,23 @@
 package xmas
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestFixedQueue_GetLen(t *testing.T) {
+	q := NewFixedQueue(5)
+
+	l := q.GetMaxLen()
+	assert.Equal(t, 5, l)
+}
+
+func TestFixedQueue_Load(t *testing.T) {
+	ns := []int{0, 1, 2, 3, 4, 5}
+	q := NewFixedQueue(3)
+	for _, n := range ns {
+		q.Load(n)
+	}
+
+	assert.Equal(t, []int{3, 4, 5}, q.Queue)
+}
