836c722250c1bb1244fafce76803c62d917216ad
Travis Reyburn <treyburn@alteryx.com>
2020-12-11 16:40:02 -0700

    implemented part 1 solution

--- Day10/cmd/main.go
+++ Day10/cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/TReyburn/advent-of-go/Day10/adapter"
+	"github.com/TReyburn/advent-of-go/common/filehandler"
+	"log"
+)
 
+func main() {
+	a := adapter.NewAdapter()
+	err := filehandler.LoadInputFile("assets/input.txt", a)
+	if err != nil {
+		log.Fatalln("Unexpected error while loading file:", err)
+	}
+
+	res, err := a.Summarize()
+	if err != nil {
+		log.Fatalln("Unexpected error while summarizing:", err)
+	}
+	fmt.Println(res)
+	fmt.Println("Jolts multiplies:", res[1]*res[3])
 }
